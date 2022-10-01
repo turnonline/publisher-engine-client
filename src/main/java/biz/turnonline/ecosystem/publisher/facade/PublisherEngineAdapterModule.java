a1f5f953641938fe4fb6625eac431df1274375ad
Aurel Medvegy <medvegy@turnonline.biz>
2022-10-01 08:56:28 +0200

    Client re-generated from API spec. #TOB-441

--- src/main/java/biz/turnonline/ecosystem/publisher/facade/PublisherEngineAdapterModule.java
+++ src/main/java/biz/turnonline/ecosystem/publisher/facade/PublisherEngineAdapterModule.java
@@ -17,14 +17,9 @@
 package biz.turnonline.ecosystem.publisher.facade;
 
 import biz.turnonline.ecosystem.publisher.facade.adaptee.EmailPayloadAdaptee;
-import biz.turnonline.ecosystem.publisher.facade.adaptee.PayInvoiceContentAdaptee;
-import biz.turnonline.ecosystem.publisher.facade.adaptee.ProductContentAdaptee;
 import biz.turnonline.ecosystem.publisher.model.EmailPayload;
-import biz.turnonline.ecosystem.publisher.model.PayInvoiceContent;
-import biz.turnonline.ecosystem.publisher.model.ProductContent;
 import com.google.inject.AbstractModule;
 import com.google.inject.TypeLiteral;
-import org.ctoolkit.restapi.client.adaptee.DeleteExecutorAdaptee;
 import org.ctoolkit.restapi.client.adaptee.InsertExecutorAdaptee;
 
 import javax.inject.Singleton;
@@ -40,16 +35,6 @@ public class PublisherEngineAdapterModule
     @Override
     protected void configure()
     {
-        // PayInvoiceContent
-        bind( new TypeLiteral<DeleteExecutorAdaptee<PayInvoiceContent>>()
-        {
-        } ).to( PayInvoiceContentAdaptee.class ).in( Singleton.class );
-
-        // ProductContent
-        bind( new TypeLiteral<DeleteExecutorAdaptee<ProductContent>>()
-        {
-        } ).to( ProductContentAdaptee.class ).in( Singleton.class );
-
         // PayInvoiceContent
         bind( new TypeLiteral<InsertExecutorAdaptee<EmailPayload>>()
         {
--- src/main/java/biz/turnonline/ecosystem/publisher/model/EmailRecipients.java
+++ src/main/java/biz/turnonline/ecosystem/publisher/model/EmailRecipients.java
@@ -1,17 +1,13 @@
 /*
- * Copyright (c) 2020 TurnOnline.biz s.r.o.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 
 package biz.turnonline.ecosystem.publisher.model;
@@ -32,32 +28,18 @@ public final class EmailRecipients
         extends com.google.api.client.json.GenericJson
 {
 
-    static
-    {
-        // hack to force ProGuard to consider EmailRecipient used, since otherwise it would be stripped out
-        // see https://github.com/google/google-api-java-client/issues/543
-        com.google.api.client.util.Data.nullOf( EmailRecipient.class );
-    }
-
-    static
-    {
-        // hack to force ProGuard to consider EmailRecipient used, since otherwise it would be stripped out
-        // see https://github.com/google/google-api-java-client/issues/543
-        com.google.api.client.util.Data.nullOf( EmailRecipient.class );
-    }
-
-    static
-    {
-        // hack to force ProGuard to consider EmailRecipient used, since otherwise it would be stripped out
-        // see https://github.com/google/google-api-java-client/issues/543
-        com.google.api.client.util.Data.nullOf( EmailRecipient.class );
-    }
-
     /**
      * The value may be {@code null}.
      */
     @com.google.api.client.util.Key
     private java.util.List<EmailRecipient> bcc;
+
+    static
+    {
+        // hack to force ProGuard to consider EmailRecipient used, since otherwise it would be stripped out
+        // see https://github.com/google/google-api-java-client/issues/543
+        com.google.api.client.util.Data.nullOf( EmailRecipient.class );
+    }
 
     /**
      * The value may be {@code null}.
@@ -65,11 +47,25 @@ public final class EmailRecipients
     @com.google.api.client.util.Key
     private java.util.List<EmailRecipient> cc;
 
+    static
+    {
+        // hack to force ProGuard to consider EmailRecipient used, since otherwise it would be stripped out
+        // see https://github.com/google/google-api-java-client/issues/543
+        com.google.api.client.util.Data.nullOf( EmailRecipient.class );
+    }
+
     /**
      * The value may be {@code null}.
      */
     @com.google.api.client.util.Key
     private java.util.List<EmailRecipient> to;
+
+    static
+    {
+        // hack to force ProGuard to consider EmailRecipient used, since otherwise it would be stripped out
+        // see https://github.com/google/google-api-java-client/issues/543
+        com.google.api.client.util.Data.nullOf( EmailRecipient.class );
+    }
 
     /**
      * @return value or {@code null} for none
